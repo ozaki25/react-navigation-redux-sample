33b64d7af682c02922ba5078cafea3e053e5075b
ozaki <fs65ugumori@yahoo.co.jp>
2017-10-02 23:03:45 +0900

    react-navigation redux sample

---
+++ index.android.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+const NavigatorRedux = () => <App />;
+
+AppRegistry.registerComponent('NavigatorRedux', () => NavigatorRedux);
